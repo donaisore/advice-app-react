ae93759e6e3b8e5f429d9f1985d6614f662a99f2
donaisore <r.h.kobe0706@gmail.com>
2020-05-04 20:11:41 +0900

    use functional componet.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 import './App.css';
 
-class App extends React.Component {
-  state = { advice: '' };
+const App = () => {
+  const[advice, setAdvice] = useState('');
 
-  componentDidMount() {
-    this.fetchAdvice();
-  }
-
-  fetchAdvice = () => {
+  const fetchAdvice = () => {
     axios.get('https://api.adviceslip.com/advice')
       .then((response) => {
-        const { advice } = response.data.slip;
-        this.setState({ advice });
+        const {advice} = response.data.slip;
+        setAdvice(advice);
       })
       .catch((error) => {
         console.log(error);
       })
-    ;
   };
 
-  render() {
-    const { advice } = this.state;
-    return (
-      <div className="app">
-        <div className="card">
-          <h1 className="heading">{advice}</h1>
-          <button className="button" onClick={this.fetchAdvice}>
-            <span>GIVE ME ADVICE!</span>
-          </button>
-        </div>
+  return (
+    <div className="app">
+      <div className="card">
+        <h1 className="heading">{advice}</h1>
+        <button className="button" onClick={fetchAdvice}>
+          <span>GIVE ME ADVICE!</span>
+        </button>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App
